6e11c7ede5fa381b5761f9517dd105e41a43d65b
Hodash26 <Florent.bailly62400@gmail.com>
2019-09-24 16:05:13 +0200

    c'est un commit utile

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+    $theme = wp_get_theme('OceanWP');
+    $version = $theme->get('Version');
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css',array('oceanwp-style'), $version);
 }
